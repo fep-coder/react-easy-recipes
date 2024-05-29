b9943127d717872a1500d918ae28653f329b5ab7
voja <voja@gmail.com>
2024-05-29 09:39:20 +0200

    09 App JSX

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,25 @@
 function App() {
-    return <div>App</div>;
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+                    <h1>Easy Recipes</h1>
+                </div>
+                <div className="col-6">
+                    <a href="#">Add new</a> |<a href="#">Log in</a> |{" "}
+                    <a href="#">Register</a>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-4">
+                    <h3>Filters</h3>
+                    <label>Search</label>
+                    <input />
+                </div>
+                <div className="col">RecipeList</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
